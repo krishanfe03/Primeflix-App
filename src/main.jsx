89891c3b146fb7f75baa08476bf8fe46790524a0
kk-Link-rgb <kunalsaini1205@gmail.com>
2025-06-23 00:04:09 +0530

    fix: prepare Primeflicx for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter basename='/PrimeflixApp'> 
+   <BrowserRouter> 
     <App/>
    </BrowserRouter>
   </StrictMode>,
